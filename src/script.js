e5675b64d76598efa093096a3247697c4794776c
taye2161 <taye2161@users.noreply.github.com>
2025-06-27 10:45:14 +0200

    adds initial frontend side of the program

---
+++ src/script.js
@@ -0,0 +1,5 @@
+const cells = document.querySelector(".cell");
+const statusText = document.querySelector("#game-step");
+const generateButton = document.querySelector("#generate-button");
+const algo = document.querySelector('input[name="radAnswer"]:checked');
+
